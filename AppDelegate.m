571154dbc5efbac5fb016562684c3c0c50ad7859
Travis Millican <travisjmillican@gmail.com>
2024-09-11 21:01:53 -0500

    Set preferred fps for the MTKView
    I'm pretty sure the default is 60 anyway, but I haven't checked.

--- AppDelegate.m
+++ AppDelegate.m
@@ -22,11 +22,17 @@
   [_window setTitle:@"Metal Window"];
   MTKView *metalView = [[MTKView alloc] initWithFrame:NSMakeRect(280, 0, 720, 720)];
   [metalView setDevice:MTLCreateSystemDefaultDevice()];
+  // NOTE: You are not guaranteed to get what you ask for. The actual maximum
+  // framerate will depend on the refresh rate of the display. See:
+  // https://developer.apple.com/documentation/metalkit/mtkview/preferredframespersecond?language=objc
+  [metalView setPreferredFramesPerSecond:60];
 
   _renderSource = [[RenderSource alloc] init];
   _renderer = [[Renderer alloc] initWithDevice:metalView.device source:_renderSource];
   metalView.delegate = _renderer;
 
+  // There's really no need for a view controller -- the events of `MTKViewDelegate`
+  // are sufficient.
   [_window.contentView addSubview:metalView];
   [_window makeKeyAndOrderFront:nil];
   [_window orderFrontRegardless];
